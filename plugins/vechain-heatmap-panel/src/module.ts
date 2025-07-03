51670b5b2e5842951a94596f5a0c68a296f1ff8e
YeahNotSewerSide <dreenot@gmail.com>
2025-07-03 15:50:59 +0300

    remove plugins

--- plugins/vechain-heatmap-panel/src/module.ts
+++
@@ -1,7 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { EpochBlocksPanel } from './components/SimplePanel';
-
-export const plugin = new PanelPlugin<SimpleOptions>(EpochBlocksPanel).setPanelOptions((builder) => {
-  return builder;
-});
