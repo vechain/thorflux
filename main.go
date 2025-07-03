b8dd379b0960f7889c4de090309bc3f1ec17be48
Pedro Gomes <otherview@gmail.com>
2025-07-03 14:57:05 +0100

    Add Sync From Block flag (#33)

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var (
 	defaultInfluxDB = "http://localhost:8086"
 	thorFlag        = flag.String("thor-url", defaultThorURL, "thor node URL, (env var: THOR_URL)")
 	blocksFlag      = flag.Uint64("thor-blocks", 360*24*7, "number of blocks to sync (best - <thor-blocks>) (env var: THOR_BLOCKS)")
+	syncBlockFlag   = flag.Uint64("sync-from-block", 0, "start sync from block height - takes precedence to thor-blocks is set (env var: SYNC_FROM_BLOCK)")
 	influxUrlFlag   = flag.String("influx-url", defaultInfluxDB, "influxdb URL, (env var: INFLUX_URL)")
 	influxTokenFlag = flag.String("influx-token", "", "influxdb auth token, (env var: INFLUX_TOKEN)")
 	influxOrg       = flag.String("influx-org", "vechain", "influxdb organization, (env var: INFLUX_ORG)")
@@ -26,7 +27,7 @@ var (
 )
 
 func main() {
-	thorURL, influxURL, influxToken, blocks, err := parseFlags()
+	thorURL, influxURL, influxToken, blocks, syncFromBlock, err := parseFlags()
 	if err != nil {
 		slog.Error("failed to parse flags", "error", err)
 		flag.PrintDefaults()
@@ -38,12 +39,24 @@ func main() {
 		"influx-org", *influxOrg,
 		"influx-bucket", *influxBucket,
 		"blocks", blocks,
+		"sync-from-block", syncFromBlock,
 	)
 
 	influx, err := influxdb.New(influxURL, influxToken, *influxOrg, *influxBucket)
 	if err != nil {
 		slog.Error("failed to create influxdb", "error", err)
 		os.Exit(1)
+	}
+
+	// if set syncFromBlock will override the thorBlocks number
+	if syncFromBlock != 0 {
+		block, err := thor.Block("best")
+		if err != nil {
+			slog.Error("failed to retrieve best block", "error", err)
+			os.Exit(1)
+		}
+		blocks = block.Number - uint32(syncFromBlock)
+		slog.Info("syncing from specified block", "syncFromBlock", syncFromBlock, "blocks", blocks)
 	}
 
 	ctx := exitContext()
@@ -66,14 +79,14 @@ func main() {
 	<-ctx.Done()
 }
 
-func parseFlags() (string, string, string, uint32, error) {
+func parseFlags() (string, string, string, uint32, uint64, error) {
 	if err := envflag.Parse(); err != nil {
-		return "", "", "", 0, err
+		return "", "", "", 0, 0, err
 	}
 
 	influxToken := *influxTokenFlag
 	if influxToken == "" {
-		return "", "", "", 0, errors.New("--influx-token or INFLUX_DB_TOKEN is required")
+		return "", "", "", 0, 0, errors.New("--influx-token or INFLUX_DB_TOKEN is required")
 	}
 
 	thorURL := *thorFlag
@@ -86,9 +99,11 @@ func parseFlags() (string, string, string, uint32, error) {
 		slog.Warn("influxdb URL not set via flag or env, using default", "url", defaultInfluxDB)
 	}
 
+	syncFromBlock := *syncBlockFlag
+
 	blocks := *blocksFlag
 
-	return thorURL, influxURL, influxToken, uint32(blocks), nil
+	return thorURL, influxURL, influxToken, uint32(blocks), syncFromBlock, nil
 }
 
 func exitContext() context.Context {
