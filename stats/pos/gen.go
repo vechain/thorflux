f74a7741410d65c08c08479c184d171b9a4de222
ShaunKav <shaun.kavanagh@vechain.org>
2025-10-20 09:17:33 +0100

    Update deprecated solidity image

--- stats/pos/gen.go
+++ stats/pos/gen.go
@@ -1,4 +1,4 @@
 package pos
 
 //go:generate curl https://raw.githubusercontent.com/vechain/thor/refs/heads/release/hayabusa/builtin/gen/staker.sol -o ./compiled/Staker.sol
-//go:generate docker run -v ./:/sources ethereum/solc:0.8.20 --evm-version paris --via-ir --overwrite --optimize --optimize-runs 200 -o /sources/compiled --abi --bin /sources/GetValidators.sol
+//go:generate docker run -v ./:/sources argotorg/solc:0.8.20 --evm-version paris --via-ir --overwrite --optimize --optimize-runs 200 -o /sources/compiled --abi --bin /sources/GetValidators.sol
